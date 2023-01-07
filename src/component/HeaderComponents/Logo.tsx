91087e2187739b64057ca07956a4797e7ecbe2c8
Orest <galenzao@gmail.com>
2023-01-07 20:15:01 +0200

    change main domen

--- src/component/HeaderComponents/Logo.tsx
+++ src/component/HeaderComponents/Logo.tsx
@@ -5,7 +5,7 @@ import Main from '../../section/Main';
 
 const Logo = () => {
     return (
-        <Link to="/main">
+        <Link to="/ranobe-lib">
             <div className='flex justify-center items-end hover:bg-opacity-[0.2]'>
                 <img className='w-40% hover:bg-opacity-5' src="https://ranobelib.me/images/ranobe.png?435" alt="" />
             </div>
