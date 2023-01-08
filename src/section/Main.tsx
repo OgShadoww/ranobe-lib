ed6820c76d6dbade6353b43a9ebd207bbe41291f
Orest <galenzao@gmail.com>
2023-01-08 08:30:04 +0200

    change gap

--- src/section/Main.tsx
+++ src/section/Main.tsx
@@ -13,12 +13,12 @@ const Main = () => {
 
     return (
         <div className='lg:px-[10%] md:px-[5%] sm:px-0 px-0 mb-[20px] lg:mt-[15px] md:mt-[15px] sm:mt-0 mt-0 w-full flex lg:justify-around justify-center gap-[20px]'>
-            <div className='flex flex-col gap-[15px] page-wrraper'>
+            <div className='flex flex-col gap-[10px] page-wrraper'>
                 <div className='flex flex-col items-start gap-[10px] main-wrapper'>
                     <h3 className='font-semibold text-[22px]'>Випадкове ранобе</h3>
                     <MainRandomList/>
                 </div>
-                <div className='flex flex-col lg:flex-row md:flex-col sm:flex-col gap-[20px]'>
+                <div className='flex flex-col lg:flex-row md:flex-col sm:flex-col gap-[10px]'>
                     <MainForumBlock title='Остальні новини' type='news'/>
                     <MainForumBlock title='Остальні теми форума' type='all'/>
                 </div>
