18ff7eccef67fe87d6db472449170db2b9e51e86
Orest <galenzao@gmail.com>
2023-01-31 08:48:08 +0200

    fix

--- src/component/Ranobe/Mobile/MobileSimilarRanobe.tsx
+++ src/component/Ranobe/Mobile/MobileSimilarRanobe.tsx
@@ -11,18 +11,24 @@ interface props {
 
 const MobileSimilarRanobe:FC<props> = ({title, items}) => {
     return (
-        <div className='text-left px-[12px] foreground snap-mandatory overflow-scroll'>
-            <div className='flex justify-between mb-[5px]'>
-                <h2 className='font-semibold'>{title}</h2>
-            </div>
-            <div className='box-border'>
-                <div className='w-fit flex duration-[0.4s] translate-x-[0px] box-border'>
-                    {items?.map(item => 
-                        <SimilarRanobeItem type='mobile' key={item.anime === null ? item.manga.id : item.anime.id} item={item}/>
-                    )}
+        <>
+            {items.length > 0 ? 
+                <div className='text-left px-[12px] foreground snap-mandatory overflow-scroll'>
+                    <div className='flex justify-between mb-[5px]'>
+                        <h2 className='font-semibold'>{title}</h2>
+                    </div>
+                    <div className='box-border'>
+                        <div className='w-fit flex duration-[0.4s] translate-x-[0px] box-border'>
+                            {items?.map(item => 
+                                <SimilarRanobeItem type='mobile' key={item.anime === null ? item.manga.id : item.anime.id} item={item}/>
+                            )}
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            : 
+                ''
+            }
+        </>
     );
 };
 
